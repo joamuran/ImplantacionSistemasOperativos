2054695a62fd669145523628fc6364f794c661a1
aberlanas <angel.berlanas@gmail.com>
2015-10-19 20:33:22 +0200

    Updated

--- pandoc-pdf.ps1
+++ pandoc-pdf.ps1
@@ -1,2 +1,8 @@
-Write-Host "Procesando Tema 1..."
-pandoc -V fontsize=14pt -V fontfamily=lmodern -o PDFS/Unidad_01.pdf Unidad_01/SistemasDeVirtualizacion.md Unidad_01/InstalacionVirtualbox.md
+Write-Host "Procesando Tema 1: ...Teoria"
+pandoc --template template-latex.latex  --toc -V fontsize=12pt -o PDFS/Unidad_01.pdf Unidad_01/SistemasDeVirtualizacion.md Unidad_01/InstalacionVirtualbox.md Unidad_01/InstalacionQEmu.md
+Write-Host "Procesando Tema 1: ...Guias"
+pandoc --template template-latex.latex --toc -V fontsize=12pt -o PDFS/GuiasInstalacion.pdf Unidad_01/InstalacionUbuntuDesktop.md Unidad_01/InstalacionUbuntuServer.md
+
+Write-Host "******"
+Write-Host "Procesando Glosarios"
+pandoc --template template-latex.latex -V fontsize=12pt -o PDFS/Glosario.pdf Unidad_01/Glosario.md
