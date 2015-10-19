d98ec00bd13623b2392f5742e104b096d9bb4e84
aberlanas <angel.berlanas@gmail.com>
2015-10-19 19:34:45 +0200

    Added builder

--- pandoc-pdf.ps1
+++ pandoc-pdf.ps1
@@ -1,2 +1,2 @@
 Write-Host "Procesando Tema 1..."
-pandoc -V fontsize=14pt -V fontfamily=lmodern -o PDFS/SistemasDeVirtualizacion.pdf Unidad_01/SistemasDeVirtualizacion.md Unidad_01/InstalacionVirtualbox.md
+pandoc -V fontsize=14pt -V fontfamily=lmodern -o PDFS/Unidad_01.pdf Unidad_01/SistemasDeVirtualizacion.md Unidad_01/InstalacionVirtualbox.md
